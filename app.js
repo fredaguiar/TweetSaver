1ed55b35cccab7ed6e590028d09f581aba4b1a08
Frederico Teixeira <fredaguiar@Fred-MacBook-Pro.local>
2017-05-31 12:37:33 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+"use strict";
+console.clear();
+
+var appClosure = function(){
+
+	const LOCAL_STORAGE_KEY = "tweets";
+	const SEP = "#@#@#";
+
+	let app = {
+		
+		init : function() {
+
+			let _this = this;
+			this.loadTweets();
+
+			$("#searchButton").on("click", function(ev){
+				let text = $("#searchText").val();
+				let endpoint = `http://tweetsaver.herokuapp.com/?q=${text}&callback=?&count=10`;
+				let endpointEncoded = encodeURI(endpoint);
+
+				$("#box1").empty();
+		   		if (text == null || text == "") {
+					return;
+				}
+				if (endpointEncoded.length > 1000) {
+					alert("Search criteria too large");
+					return false;
+				}
+
+				$.getJSON(endpointEncoded, function(data) {
+					for (let tweet of data.tweets) {
+						let datefmt = new Date(tweet.createdAt).toISOString().slice(0,10);
+						this.addRow($("#box1"), tweet.user.name, tweet.user.miniProfileImageURL, datefmt, tweet.text, true);
+					}
+				}.bind(_this)); // or _this.addRow instead.
+			});
+		},
+
+		addRow : function(box, name, image, date, text, isDraggable) {
+
+			let block = `<div class="tweetItem" draggable="${isDraggable}" ondragstart="appClosure.dragStart(event)"> 
+		    			<div class="tweetItemCol"><img id="picture" src="${image}"></div> 
+		    			<div class="tweetItemCol"> 
+			    			<div class="tweetItemCol2"> 
+			    				<div class="tweetMessageCol"> 
+			    					<div class=""> 
+			    						<span style="font-weight: 700; padding-right: 3px;">Full Name</span>  
+			    						@<span id="fullname">${name}</span><br> 
+			    					</div> 
+			    					<div id="date">${date}</div> 
+			    				</div> 
+			    				<div id="result">${text}</div> 
+			    			</div> 
+			    		</div> 
+	    			</div>`;
+
+		    $(block).appendTo(box);
+		},
+
+		loadTweets : function() {
+
+			$("#box2").empty();
+
+			let storedTweets = localStorage.getItem(LOCAL_STORAGE_KEY);
+			if (storedTweets == null) {
+				return;
+			}
+
+			let storedTweetsArr = storedTweets.split(SEP);
+			for (let item of storedTweetsArr) {
+				let tweet = JSON.parse(item);
+				this.addRow($("#box2"), tweet.name, tweet.image, tweet.date, tweet.text, false);
+			}
+		},
+
+		drop : function(ev) {
+			
+			let tweet = ev.dataTransfer.getData('text');
+
+			let storedTweets = localStorage.getItem(LOCAL_STORAGE_KEY);
+			let storedTweetsArr = [];
+			if (storedTweets != null) {
+				storedTweetsArr = storedTweets.split(SEP);
+			}
+			storedTweetsArr.push(tweet);
+
+			var toStorage = storedTweetsArr.join(SEP);
+			localStorage.setItem(LOCAL_STORAGE_KEY, toStorage);
+			this.loadTweets();
+			$("#box2").css({"background-color":"#eee", "border":"1px solid #eee"});
+		    ev.preventDefault();
+		},
+
+		dragOver : function(ev) {
+			$("#box2").css({"background-color":"lightyellow", "border":"1px solid red"});
+	    	ev.preventDefault();
+		},
+
+		dragLeave : function(ev) {
+			$("#box2").css({"background-color":"#eee", "border":"1px solid #eee"});
+	    	ev.preventDefault();
+		},
+
+		dragStart : function(ev) {
+			let date = $(ev.target).find("#date").text();
+			let image = $(ev.target).find("#picture").attr('src');
+			let name = $(ev.target).find("#fullname").text();
+			let text = $(ev.target).find("#result").text();
+		    ev.dataTransfer.setData('text', JSON.stringify({date: date, name: name, image: image, text : text}));
+		}
+
+	};
+
+	return app;
+}(); // IIFE - Imemediately invoked function expression
+
+window.onload = appClosure.init();
+
